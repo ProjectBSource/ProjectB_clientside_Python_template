ceaee0c8667cedcbe7907f1a580ba36b46941d06
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-10-27 16:54:39 +0800

    Update TradeController.py

--- src/ProjectB_clientside_template_package/TradeControl/TradeController.py
+++ src/ProjectB_clientside_template_package/TradeControl/TradeController.py
@@ -42,9 +42,10 @@ class TradeController:
     def placeOrder(self, symbol: str, action: str, quantity: int, direction=None, sp=None, ed=None):
         self.orders.append(Order(symbol, action, direction, sp, ed, quantity, None, None, None, None, False))
 
-    def placeOrder(self, symbol: str, action: str, direction=None, sp=None, ed=None):
+   def placeOrder(self, symbol: str, action: str, direction=None, sp=None, ed=None):
         self.tempOffQuantity = self.profile.holding[symbol]*-1
-        self.orders.append(Order(symbol, action, direction, sp, ed, self.tempOffQuantity, None, None, None, None, False))
+        if self.tempOffQuantity != 0:
+            self.orders.append(Order(symbol, action, direction, sp, ed, self.tempOffQuantity, None, None, None, None, False))
     
     def getProfile(self):
         profile_dict = self.profile.__dict__
