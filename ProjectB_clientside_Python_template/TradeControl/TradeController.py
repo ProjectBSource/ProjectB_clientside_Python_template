8d385e6ee306de3a70ed45d59600024bdc4babfc
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-11-13 11:41:39 +0800

    Update TradeController.py

--- ProjectB_clientside_Python_template/TradeControl/TradeController.py
+++ ProjectB_clientside_Python_template/TradeControl/TradeController.py
@@ -18,11 +18,22 @@ class TradeController:
     
     def tradeCheckingAndBalanceUpdate(self, ds: DataStructure):
         self.trade_notification_list = []
+        self.trade_notification = None
+
         for order in self.orders:
             self.trade_notification = order.trade(self.profile, ds, self.slippage)
             if self.trade_notification is not None:
                 self.trade_notification_list.append(self.trade_notification)
         
+        # Move the completed OFF trade to completedOrders arraylist
+        tempNewOrders = self.orders.copy()
+        for order in self.orders.items():
+            if "_OFF" in order[0]:
+                if order[1].remained == 0:
+                    self.completedOrders.append(order[1])
+                    tempNewOrders.pop(order[0])
+        self.orders = tempNewOrders
+
         # update profile balance
         self.profile.balance = 0.0
         for symbol, quantity in self.profile.holding.items():
@@ -41,24 +52,25 @@ class TradeController:
     
     def placeOrder(self, id: str, symbol: str, action: str, quantity: int, direction, sp, ed, oneTimeTradeCheck):
         if id not in self.orders:
-            self.orders[id] = Order(symbol, action, direction, sp, ed, quantity, None, None, None, None, False, oneTimeTradeCheck)
-            return False
-        return True
+            self.orders[id] = Order(id, symbol, action, direction, sp, ed, quantity, None, None, None, None, False, oneTimeTradeCheck)
+            return True
+        return False
 
-    def placeOFFOrder(self, targetId: str):
+    def placeOFFOrder(self, targetId: str, dataStructure: DataStructure):
         order = self.orders.get(targetId)
         if order:
             if self.profile.holding:
                 if order.symbol in self.profile.holding:
                     if self.profile.holding[order.symbol] >= order.traded:
-                        # For non option trade off
+                        # For All trade off
                         if order.action == Action.BUY:
-                            self.orders[targetId + "_OFF"] = Order(symbol, Action.SELL, direction, sp, ed, order.traded, None, None, None, None, False)
+                            self.orders[targetId + "_OFF"] = Order(targetId+"_OFF", order.symbol, Action.SELL, order.direction, order.sp, order.ed, order.traded, None, None, None, None, False, order.oneTimeTradeCheck)
                         elif order.action == Action.SELL:
-                            self.orders[targetId + "_OFF"] = Order(symbol, Action.BUY, direction, sp, ed, order.traded, None, None, None, None, False)
-                        return True
+                            self.orders[targetId + "_OFF"] = Order(targetId+"_OFF", order.symbol, Action.BUY,  order.direction, order.sp, order.ed, order.traded, None, None, None, None, False, order.oneTimeTradeCheck)
                         if order.oneTimeTradeCheck==False:
+                            self.completedOrders.append(order)
                             self.orders.pop(targetId)
+                        return True
         return False
 
     def getProfile(self):
